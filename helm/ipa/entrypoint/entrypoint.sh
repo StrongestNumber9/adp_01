b9650fe84e04b9253ae449bf8e6fa1bd0ed6b699
StrongestNumber9 <16169054+StrongestNumber9@users.noreply.github.com>
2025-07-03 11:57:15 +0300

    Makes ipa survive minikube restart, adds new reverse dnszone to hopefully fix spilling to next subnet

--- helm/ipa/entrypoint/entrypoint.sh
+++ helm/ipa/entrypoint/entrypoint.sh
@@ -6,11 +6,15 @@ if [ ! -d /data/backup ]; then
     exit 1;
 fi;
 
-
 # Asks for password and confirmation.
 echo "Init already done, restoring from backup..";
 echo -e "{{.Values.ipa.password}}\ny" | ipa-restore /data/backup;
 echo "Backup restored, following the logs.";
+
+# Required to survive Minikube restart. No modifications results in error so running '|| true' is easier than checking whether current ip is what is configured.
+echo "Attempting to set a new DNS entry for IPA";
+echo "{{.Values.ipa.password}}" | kinit admin;
+ipa dnsrecord-mod tg.dev.test ipa --a-rec="$(hostname -I)" 2>/dev/null || true;
 
 # Additional check for liveness
 touch /ipa.ready;
